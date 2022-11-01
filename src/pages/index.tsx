d29ceea7e6ca0a08e4ccafefc88b496b4c419cdb
Mvery <matheus.verissimo@outlook.com>
2022-11-01 19:07:56 -0300

    feat: home page + global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
+import Image from "next/image"
 import { styled } from "../styles"
+import { HomeContainer, Product } from "../styles/pages/home"
+
+import camiseta1 from '../assets/Variant6.png'
+import camiseta2 from '../assets/Variant7.png'
+import camiseta3 from '../assets/Variant8.png'
 
 const Button = styled('button', {
   backgroundColor: 'PeachPuff'
@@ -6,8 +12,16 @@ const Button = styled('button', {
 
 export default function Home() {
   return (
-    <Button>
-      Hello
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt=""/>
+        {/* <Image src={camiseta2} width={520} height={480} alt=""/> */}
+        {/* <Image src={camiseta3} width={520} height={480} alt=""/> */}
+        <footer>
+          <strong></strong>
+          <span></span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
--- src/pages/teste.tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router"
-
-export default function Teste() {
-  const { query } = useRouter()
-  return (
-    <div>teste</div>
-  )
-}

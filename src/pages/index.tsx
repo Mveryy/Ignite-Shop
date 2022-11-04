648b28f75cb814481b5aad7d054ecf7471b3a819
Mvery <matheus.verissimo@outlook.com>
2022-11-04 18:42:40 -0300

    feat: implement getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
-import Image from "next/image"
-import { styled } from "../styles"
-import { HomeContainer, Product } from "../styles/pages/home"
-import {useKeenSlider} from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+import Image from "next/image"
+import { HomeContainer, Product } from "../styles/pages/home"
 
-import camiseta1 from '../assets/Variant6.png'
-import camiseta2 from '../assets/Variant7.png'
-import camiseta3 from '../assets/Variant8.png'
-import { stripe } from "../lib/stripe"
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Stripe from "stripe"
+import { stripe } from "../lib/stripe"
 
 interface HomeProps {
   products: {
@@ -45,7 +41,7 @@ export default function Home({products}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -56,12 +52,16 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount / 100),
     }
   })
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2 // 2 hours
   }
 }
